969e438d2f98249f65246cd584b780ce74f67312
matthewlefler <matthewlefler1@gmail.com>
2024-10-03 15:45:32 -0400

    devcade game setup

---
+++ devcade-game-template-main/DevcadeGame/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Devcade;
+
+namespace EntityClass
+{
+    class Entity
+    {
+
+    #region parameters
+        
+
+    #endregion
+
+        public Entity()
+        {
+
+        }
+    }
+}
---
+++ devcade-game-template-main/DevcadeGame/Scripts/Program.cs
@@ -0,0 +1,3 @@
+﻿// This file does not need to be edited.
+using var game = new RacingGame.Game1();
+game.Run();
